c24254ca3bc658b8dfbd11c200c1e5157cff41db
Bloodov Pavel <kasper6024@gmail.com>
2023-05-10 23:18:18 +0300

    download book covers.

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 import os
 import requests
 from pathlib import Path
+from os.path import split, splitext
 from pathvalidate import sanitize_filename
 from requests import HTTPError
 from bs4 import BeautifulSoup
+from urllib.parse import urljoin, urlsplit, unquote
+
+
+def get_filename_and_ext(img_url):
+    """Getting the link address and extension"""
+    url_address = urlsplit(img_url).path
+    encoding_url = unquote(url_address)
+    filename = split(encoding_url)[-1]
+    extension = splitext(filename)[-1]
+    return filename, extension
 
 
 def check_for_redirect(response):
@@ -30,15 +41,48 @@ def download_txt(url, filename, folder='books/'):
         file.write(response.content)
 
 
-def get_book_name(book_id):
+def download_image(url, filename, folder='images/'):
+    """Функция для скачивания изображений.
+    Args:
+        url (str): Cсылка на картинку, которую хочется скачать.
+        filename (str): Имя файла, с которым сохранять.
+        folder (str): Папка, куда сохранять.
+    Returns:
+        str: Путь до файла, куда сохранён текст.
+    """
+    book_path = Path(folder)
+    book_path.mkdir(parents=True, exist_ok=True)
+    response = requests.get(url)
+    response.raise_for_status()
+    normal_filename = sanitize_filename(filename)
+    file_path = os.path.join(folder, normal_filename)
+    with open(f'{file_path}', 'wb') as file:
+        file.write(response.content)
+
+
+def get_soup(book_id):
     url = f'https://tululu.org/b{book_id}/'
     response = requests.get(url)
     response.raise_for_status()
     soup = BeautifulSoup(response.text, 'lxml')
+    return soup
+
+
+def get_book_name(book_id):
+    soup = get_soup(book_id)
     title_tag = soup.find('td', class_='ow_px_td').find('div', id='content').find('h1')
     book_title = title_tag.text.split('::')[0].strip()
     book_name = f'{book_id}.{book_title}.txt'
     return book_name
+
+
+def get_img_url_name(book_id):
+    url = 'https://tululu.org/'
+    soup = get_soup(book_id)
+    img = soup.find('div', class_='bookimage').find('img')['src']
+    img_url = urljoin(url, img)
+    img_name, _ = get_filename_and_ext(img_url)
+    return img_url, img_name
 
 
 def fetch_books(num):
@@ -48,12 +92,17 @@ def fetch_books(num):
         try:
             url = 'https://tululu.org/txt.php'
             params = {'id': book_id}
-            response = requests.get(url, params=params)
+            response = requests.get(url, params=params, allow_redirects=False)
             check_for_redirect(response)
             response.raise_for_status()
+
             book_url = response.url
             book_name = get_book_name(book_id)
             download_txt(book_url, book_name)
+
+            img_url, img_name = get_img_url_name(book_id)
+            download_image(img_url, img_name)
+
             book_id += 1
             fetch_book += 1
         except (HTTPError, AttributeError):
