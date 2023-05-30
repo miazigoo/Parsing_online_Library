077ccfe6b7620ec9d4ba9028066fa87ed5f77b1e
Bloodov Pavel <kasper6024@gmail.com>
2023-05-30 19:33:00 +0300

    Integrate livereload.

--- render_website.py
+++ render_website.py
@@ -1,7 +1,15 @@
 import json
-from http.server import HTTPServer, SimpleHTTPRequestHandler
+import logging
 
+from livereload import Server, shell
 from jinja2 import Environment, FileSystemLoader, select_autoescape
+
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(
+        level=logging.INFO,
+        format="%(levelname)s - %(message)s",
+    )
 
 with open("Parse/books_INFO/books_INFO_page_1_10.json", "r", encoding="utf-8") as my_file:
     books_json = my_file.read()
@@ -12,14 +20,20 @@ env = Environment(
     loader=FileSystemLoader('.'),
     autoescape=select_autoescape(['html'])
 )
-template = env.get_template('index.html')
 
-rendered_page = template.render(
-    books=books
-)
 
-with open('pages/index.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
+def on_reload():
+    template = env.get_template('base.html')
+    rendered_page = template.render(
+        books=books
+    )
 
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
+    logger.error("Работает (~_~)=/")
+
+
+server = Server()
+server.watch('pages/*.rst', shell('make html', cwd='pages'), on_reload())
+server.watch("base.html", on_reload)
+server.serve(root='.')
