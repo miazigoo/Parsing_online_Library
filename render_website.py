5d3b173c9503431536f490611d7992343b24f6a7
Bloodov Pavel <kasper6024@gmail.com>
2023-06-14 16:59:11 +0300

    Добавлено: Изменение структуры

--- render_website.py
+++ render_website.py
@@ -15,14 +15,17 @@ logging.basicConfig(
     format="%(levelname)s - %(message)s",
 )
 
+environs = Env()
+environs.read_env()
+BOOKS_PAGE_PATH = environs.str("BOOKS_PAGES", "books_page.json")
 
-def on_reload(environs):
-    books_page_path = environs.str("BOOKS_PAGES", "books_page.json")
+
+def on_reload():
     env = Environment(
         loader=FileSystemLoader("."),
         autoescape=select_autoescape(["html"])
     )
-    with open(books_page_path, "r", encoding="utf-8") as my_file:
+    with open(BOOKS_PAGE_PATH, "r", encoding="utf-8") as my_file:
         books = json.load(my_file)
     books_on_page = 10
     count_pages = math.ceil(len(books) / books_on_page)
@@ -43,11 +46,9 @@ def on_reload(environs):
 
 
 def main():
-    environs = Env()
-    environs.read_env()
     logger.info('Скрипт запущен')
     server = Server()
-    server.watch("pages/*.rst", shell("make html", cwd="pages"), on_reload(environs))
+    server.watch("pages/*.rst", shell("make html", cwd="pages"), on_reload())
     server.watch("template/base.html", on_reload)
     server.serve(open_url_delay=5, debug=False, default_filename="pages/index_1.html")
 
